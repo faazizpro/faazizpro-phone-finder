86c5d91b77c3eae889a498bae3666ea7c4f21074
faazizpro <fahadafs11@gmail.com>
2024-01-02 01:00:13 +0600

    Fetched & showed all phones

--- JS/phone.js
+++ JS/phone.js
@@ -6,8 +6,22 @@ const loadPhone = async() => {
 };
 
 const displayPhone = phones => {
+    const phoneContainer = document.getElementById('phone-container');
     phones.forEach(phone =>{
         console.log(phone);
+        // 2. Create a div
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card max-w-96 mx-auto bg-base-100 shadow-2xl p-5`;
+        phoneCard.innerHTML =  `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body flex flex-col items-center">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p class="text-center">There are many variations of passages of available, but the majority have suffered</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary bg-blue-500 text-white">Show Details</button>
+          </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
     })
 
 }
